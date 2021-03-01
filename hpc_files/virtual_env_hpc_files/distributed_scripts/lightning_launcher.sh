df00519e9494093ab33d63a43bb5ecf855475e3c
Andreas Munk <andreas@ammunk.com>
2021-02-28 17:12:46 -0800

    update hpc files

--- hpc_files/virtual_env_hpc_files/distributed_scripts/lightning_launcher.sh
+++ hpc_files/virtual_env_hpc_files/distributed_scripts/lightning_launcher.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 ## for torch distributed launch
 nnodes=$1               # total number of nodes used in this computation
-nproc_per_node=$2       # number of processes (models) per node
+nproc_per_node=$2       # number of processes (models) per node (typically equal number of gpus per node)
 tarball=$3              # tarball containing data etc to be moved to local node
 
+# cleanup specified command
+program="$(cut -d ' ' -f1 <<< "${cmd}")"
+if [[ ! ${program} == "python"* ]]; then
+    echo "Command must be a python execution" >&2 ; exit 1
+fi
+cmds="$(cut -d ' ' -f2- <<< "${cmd}" | sed -e 's/ .*num_nodes.* / /g' -e 's/ .*gpus.* / /g')"
+cmds="$(sed -r  sed -e 's/ .*num_nodes.*//g' -e 's/ .*gpus.*//g' <<< "${cmds}")"
+cmd="python ${cmds} --num_nodes=${nnodes} --gpus=${nproc_per_node}"
 IFS=', ' read -r -a cmd <<< "${cmd}"
 # create plai machine temporary directory
 if [[ "${SLURM_TMPDIR}" == *"scratch-ssd"* ]]; then
@@ -16,7 +24,4 @@ if [ ! -z ${tarball}  ]; then
         -C ${SLURM_TMPDIR} --strip-components=$(wc -w <<< $(tr "/" " " <<< ${scratch_dir}))
 fi
 
-batch_size=32 # batch size for each processes
-precision=16 # set floating point precision
-
-python "${cmd[@]}"
+"${cmd[@]}"
